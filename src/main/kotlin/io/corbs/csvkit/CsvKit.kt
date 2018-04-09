be98a2fe8b9ff1c6956286b7d9e48cde035e71d4
Corbett Martin <corbtastik@gmail.com>
2018-04-09 15:03:55 -0500

    adding eureka client support

--- src/main/kotlin/io/corbs/csvkit/CsvKit.kt
+++ src/main/kotlin/io/corbs/csvkit/CsvKit.kt
@@ -2,7 +2,11 @@ package io.corbs.csvkit
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
 
+@EnableHystrix
+@EnableCircuitBreaker
 @SpringBootApplication
 class CsvKit
 
